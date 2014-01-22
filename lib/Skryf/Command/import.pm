0b9871ab914c836d69bc14e849cc041559658303
Adam Stokes <adam.stokes@ubuntu.com>
2014-01-21 23:43:18 -0500

    remove requirement on model::blog
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Skryf/Command/import.pm
+++ lib/Skryf/Command/import.pm
@@ -1,62 +1,69 @@
 package Skryf::Command::import;
 
-use Mojo::Base 'Mojolicious::Command';
-use FindBin '$Bin';
-use feature qw[say];
-use Carp;
-use Path::Tiny;
-use Skryf::Model::Blog;
-use DateTime::Format::RFC3339;
-
-our $VERSION = '1.0.1';
-
-has description => "Import blog posts from another service\n";
-has usage       => <<"EOF";
-
-Usage: $0 import [service] [static posts]
-
-[jekyll|octopress|hexo] service is required
-  Supported Services:
-    - jekyll/octopress
-
-EOF
-
-sub format_date {
-    my ($self, $date) = @_;
-    my $dt = DateTime::Format::RFC3339->new;
-    if ($date !~ /\d+:\d+:\d+Z/) {
-        return $dt->parse_datetime($date . 'T00:00:59Z');
-    }
-    return $dt->parse_datetime($date);
-}
-
-sub run {
-    my ($self, $service, @args) = @_;
-    my $model = Skryf::Model::Blog->new;
-    croak($self->usage) unless $service =~ /skryf|jekyll/;
-
-    if ($service eq lc "jekyll" || $service eq lc "octopress") {
-        croak('Needs -d [posts] directory defined') unless $args[1];
-        my $work_dir = path($args[1]);
-        say "Starting import of posts in $args[1] ... Please wait.";
-        for ($work_dir->children) {
-            my $post      = $_->slurp_utf8;
-            my ($topic)   = $post =~ /^Topic:\s+(.*)/;
-            my ($date)    = $post =~ /Date:\s+(.*)/;
-            my ($tags)    = $post =~ /Category:\s+(.*)/;
-            my ($content) = $post =~ /^\n(.*)/ms;
-            say "processing $topic";
-            $model->create($topic, $content, $tags,
-                $self->format_date($date));
-        }
-        say "Import complete";
-    }
-    elsif ($service eq lc "hexo") {
-        croak "Not implemented.";
-    }
-    else {
-        croak "Unknown service: $service";
-    }
-}
 
 1;
+
+__END__
+
+=head1 EXAMPLE
+
+    use Mojo::Base 'Mojolicious::Command';
+    use FindBin '$Bin';
+    use feature qw[say];
+    use Carp;
+    use Path::Tiny;
+    use Skryf::Model::Blog;
+    use DateTime::Format::RFC3339;
+    
+    our $VERSION = '1.0.1';
+    
+    has description => "Import blog posts from another service\n";
+    has usage       => <<"EOF";
+    
+    Usage: $0 import [service] [static posts]
+    
+    [jekyll|octopress|hexo] service is required
+      Supported Services:
+        - jekyll/octopress
+    
+    EOF
+    
+    sub format_date {
+        my ($self, $date) = @_;
+        my $dt = DateTime::Format::RFC3339->new;
+        if ($date !~ /\d+:\d+:\d+Z/) {
+            return $dt->parse_datetime($date . 'T00:00:59Z');
+        }
+        return $dt->parse_datetime($date);
+    }
+    
+    sub run {
+        my ($self, $service, @args) = @_;
+        my $model = Skryf::Model::Blog->new;
+        croak($self->usage) unless $service =~ /skryf|jekyll/;
+    
+        if ($service eq lc "jekyll" || $service eq lc "octopress") {
+            croak('Needs -d [posts] directory defined') unless $args[1];
+            my $work_dir = path($args[1]);
+            say "Starting import of posts in $args[1] ... Please wait.";
+            for ($work_dir->children) {
+                my $post      = $_->slurp_utf8;
+                my ($topic)   = $post =~ /^Topic:\s+(.*)/;
+                my ($date)    = $post =~ /Date:\s+(.*)/;
+                my ($tags)    = $post =~ /Category:\s+(.*)/;
+                my ($content) = $post =~ /^\n(.*)/ms;
+                say "processing $topic";
+                $model->create($topic, $content, $tags,
+                    $self->format_date($date));
+            }
+            say "Import complete";
+        }
+        elsif ($service eq lc "hexo") {
+            croak "Not implemented.";
+        }
+        else {
+            croak "Unknown service: $service";
+        }
+    }
+
+=cut
