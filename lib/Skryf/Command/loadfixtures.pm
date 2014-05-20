0d250c226c47a00bb5d783a3364f032ca1711cff
Adam Stokes <adam.stokes@ubuntu.com>
2014-05-20 17:00:23 -0400

    use fixtures
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Skryf/Command/loadfixtures.pm
+++ lib/Skryf/Command/loadfixtures.pm
@@ -3,24 +3,29 @@ package Skryf::Command::loadfixtures;
 
 use Mojo::Base 'Mojolicious::Commands';
 use Mojo::JSON qw(decode_json encode_json);
+use Mojo::Util qw(hmac_sha1_sum);
 use Skryf::DB;
 use Path::Tiny;
 use Carp;
 
 has description => "Load fixtures into db\n";
-has usage       => "usage: $0 loadfixtures [DBNAME]\n";
+has usage       => "usage: $0 loadfixtures [DBNAME] <fixtures.json>\n";
 
 sub run {
     my $self = shift;
     my $dbname = $_[0] || undef;
+    my $fixturepath = $_[1] || undef;
     croak "Specify a DBNAME" unless defined $dbname;
-    my $fixtures = path("fixtures/users.json")->slurp_utf8;
+    croak "Specify a fixture path" unless defined $fixturepath;
+    my $fixtures = path($fixturepath)->slurp_utf8;
     my $json     = decode_json($fixtures);
     my $db       = Skryf::DB->new(dbname => $dbname);
-    my $users    = $self->db->namspace('users');
+    my $users    = $db->namespace('users');
     for my $i (@{$json->{users}}) {
         say "Adding user: " . $i->{username};
-        $self->users->insert($i);
+        $i->{password} =
+          hmac_sha1_sum('this sentence would be a secret.', $i->{password});
+        $users->insert($i);
     }
 
 }
